79a8756cd3f50d44f4db747abc3457daaf2cf0b0
Coby <contact@45930.xyz>
2023-02-19 16:41:53 -0500

    Simple copy on home page (#1)

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex">
+	<div class="mt-6 mb-10 h-fit text-center w-1/2 mx-auto">
+		<h2 class="text-2xl font-semibold">
+			Mina Arena is a (truly) free-to-play, multiplayer blockchain game where you control your team
+			in the arena in turn-based combat against an opponent.
+		</h2>
+	</div>
+</div>
+<div class="flex">
+	<a href="/enter" class="py-10 px-16 border border-slate-800 mx-auto">Enter the Arena!</a>
+</div>
---
+++ client/src/routes/enter/+page.svelte
@@ -0,0 +1,21 @@
+<div class="flex">
+	<div class="mt-6 mb-10 h-fit text-center w-1/2 mx-auto">
+		<h2 class="text-2xl font-semibold">Choose your path</h2>
+	</div>
+</div>
+<div class="grid grid-cols-2 gap-20 w-3/4 border border-slate-100 mx-auto">
+	<div class="text-center card p-10 rounded border border-slate-500">
+		<h3 class="text-xl mb-3">Sandbox</h3>
+		<p class="text-slate-600">
+			In sandbox mode, you control both sides. It's a good way to learn the game or practice a
+			strategy. Gameplay is fast and no proofs are generated.
+		</p>
+	</div>
+	<div class="text-center card p-10 rounded border border-slate-500">
+		<h3 class="text-xl mb-3">Play with a Friend</h3>
+		<p class="text-slate-600">
+			In multiplayer mode, you will only control your own pieces. Turns will take time to prove, but
+			the result of the game will be proved and tracked on the Mina blockchain!
+		</p>
+	</div>
+</div>
