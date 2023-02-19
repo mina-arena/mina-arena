3df1684b1dd45e55bc9bab816b3540fc3843f406
Coby <contact@45930.xyz>
2023-02-19 18:06:31 -0500

    wireframe team draft

--- client/src/routes/enter/+page.svelte
+++ client/src/routes/enter/+page.svelte
@@ -4,13 +4,13 @@
 	</div>
 </div>
 <div class="grid grid-cols-2 gap-20 w-3/4 border border-slate-100 mx-auto">
-	<div class="text-center card p-10 rounded border border-slate-500">
+	<a href="/sandbox" class="text-center card p-10 rounded border border-slate-500">
 		<h3 class="text-xl mb-3">Sandbox</h3>
 		<p class="text-slate-600">
 			In sandbox mode, you control both sides. It's a good way to learn the game or practice a
 			strategy. Gameplay is fast and no proofs are generated.
 		</p>
-	</div>
+	</a>
 	<div class="text-center card p-10 rounded border border-slate-500">
 		<h3 class="text-xl mb-3">Play with a Friend</h3>
 		<p class="text-slate-600">
---
+++ client/src/routes/sandbox/new/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	let playerTurn = 1;
+	let currentSquad: Set<number> = new Set();
+	const eligibleUnits = [...Array(25).keys()];
+
+	const squads: Record<number, Set<number>> = {};
+
+	const selectSquad = () => {
+		squads[playerTurn] = currentSquad;
+		currentSquad = new Set();
+		playerTurn++;
+	};
+</script>
+
+<div class="text-center">
+	<h2 class="text-2xl font-semibold mb-10">Sandbox Mode</h2>
+	<div>
+		<h3 class="text-xl">Select your squad</h3>
+		<p>Drafting for Player{playerTurn}</p>
+		<div class="grid grid-cols-5 gap-6 w-3/4 mx-auto">
+			{#each eligibleUnits as unit}
+				<div class="border border-slate-300 rounded py-6 col-span-1">{unit}</div>
+			{/each}
+		</div>
+		<button on:click={selectSquad}>Select Squad</button>
+	</div>
+</div>
