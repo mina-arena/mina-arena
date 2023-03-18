3aa666b562d85c094d56cd405fe13ea93b395228
Coby <contact@45930.xyz>
2023-03-18 00:01:21 -0400

    render pieces to move

--- client/src/lib/components/sandbox/phase-input/MovementPhaseInput.svelte
+++ client/src/lib/components/sandbox/phase-input/MovementPhaseInput.svelte
@@ -2,4 +2,20 @@
 	export let game: Game;
 </script>
 
-<div>Movement Phase Input</div>
+<div>
+	<p>Movement Phase Input</p>
+	<table>
+		<tr>
+			<th>Piece</th>
+			<th>From</th>
+			<th>To</th>
+		</tr>
+		{#each game.gamePieces || [] as piece}
+			<tr>
+				<td>{piece.playerUnit.name || 'Bob'} the {piece.playerUnit.unit.name}</td>
+				<td>{piece.coordinates.x}, {piece.coordinates.y}</td>
+				<td>??</td>
+			</tr>
+		{/each}
+	</table>
+</div>
