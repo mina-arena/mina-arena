e02359b3e578172a07e8f393d1a8ec582ee8c712
Coby <contact@45930.xyz>
2023-07-07 15:59:48 -0400

    fix redirection

--- client/src/routes/sandbox/new/+page.server.ts
+++ client/src/routes/sandbox/new/+page.server.ts
@@ -1,11 +1,10 @@
-import { redirect } from '@sveltejs/kit';
 import { MinaArenaClient } from '$lib/mina-arena-graphql-client/MinaArenaClient';
 import { dummyPlayer1, dummyPlayer2 } from '$lib/stores/sandbox/playerStore';
+import { redirect } from '@sveltejs/kit';
 
-/** @type {import('./$types').PageServerLoad} */
-export function load() {
-  const minaArenaClient = new MinaArenaClient();
-  return {
-    game: Promise.resolve(minaArenaClient.createGame([dummyPlayer1, dummyPlayer2])),
-  };
+export async function load() {
+	const minaArenaClient = new MinaArenaClient();
+	const game = await minaArenaClient.createGame([dummyPlayer1, dummyPlayer2]);
+	console.log(game.id);
+	throw redirect(302, `/sandbox/games/${game.id}`);
 }
--- client/src/routes/sandbox/new/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { redirect } from '@sveltejs/kit';
-
-	export let data;
-
-	export function load() {
-		throw redirect(302, `/sandbox/games/${data.game.id}`);
-	}
-</script>
