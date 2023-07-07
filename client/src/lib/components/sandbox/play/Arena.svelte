dcd2d8b8db628b5172fa296069e75e545979b12c
Coby <contact@45930.xyz>
2023-07-07 16:23:10 -0400

    fix legend

--- client/src/lib/components/sandbox/play/Arena.svelte
+++ client/src/lib/components/sandbox/play/Arena.svelte
@@ -1,41 +1,35 @@
 <script lang="ts">
-  import ArenaCanvas from '../arena/ArenaCanvas.svelte';
-  import ArenaLegend from '../arena/ArenaLegend.svelte';
-  import ArenaActionLog from '../arena/ArenaActionLog.svelte';
+	import ArenaCanvas from '../arena/ArenaCanvas.svelte';
+	import ArenaLegend from '../arena/ArenaLegend.svelte';
+	import ArenaActionLog from '../arena/ArenaActionLog.svelte';
 
-  export let game: Game;
+	export let game: Game;
 
-  const legendConfig = {
-    colors: ['pink', 'lightblue']
-  };
+	const legendConfig = {
+		colors: ['#EF626C', '#84DCCF']
+	};
 
-  const playerPublicKeys = (game.gamePlayers || []).map((p) => p.player.minaPublicKey) || ['', ''];
+	const playerPublicKeys = (game.gamePlayers || []).map((p) => p.player.minaPublicKey) || ['', ''];
 
-  export let rerender: () => {};
+	export let rerender: () => {};
 </script>
 
 <div class="flex">
-  <ArenaCanvas
-    game={game}
-    playerColors={legendConfig.colors}
-    rerender={rerender}
-  />
-  <table>
-    <tr>
-      <ArenaLegend
-        playerPublicKeys={playerPublicKeys}
-        playerColors={legendConfig.colors}
-      />
-    </tr>
-    <tr>
-      <ArenaActionLog
-        game={game}
-        playerPublicKeys={playerPublicKeys}
-        playerColors={legendConfig.colors}
-      />
-    </tr>
-  </table>
+	<ArenaCanvas {game} playerColors={legendConfig.colors} {rerender} />
+	<table>
+		<tr>
+			<ArenaLegend {playerPublicKeys} playerColors={legendConfig.colors} />
+		</tr>
+		<tr>
+			<ArenaActionLog {game} {playerPublicKeys} playerColors={legendConfig.colors} />
+		</tr>
+	</table>
 </div>
 <div style="display: none;">
-  <img id="arena-bg-img" src="/images/battlefield_dirt_650x550.png" width={game.arena.width} height={game.arena.height} />
+	<img
+		id="arena-bg-img"
+		src="/images/battlefield_dirt_650x550.png"
+		width={game.arena.width}
+		height={game.arena.height}
+	/>
 </div>
