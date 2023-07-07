8ac54b2f33eee21fced49526da1f90f59fbffbd5
Coby <contact@45930.xyz>
2023-07-07 12:59:07 -0400

    add theme and instructions page

---
+++ client/src/routes/how-to-play/+page.svelte
@@ -0,0 +1,49 @@
+<div class="container">
+	<div class="flex">
+		<div class="mt-6 mb-10 h-fit text-center mx-auto">
+			<h1 class="text-2xl font-semibold">How to play Mina Arena</h1>
+		</div>
+	</div>
+	<div class="flex">
+		<div class="mt-6 mb-10 h-fit text-center mx-auto">
+			<h2 class="text-xl font-semibold">Squad Draft</h2>
+			Before starting a game, you will draft your squad. Each unit in your squad has certain stats, strengths
+			and weaknesses. Each unit also has a cost, and the total cost of your team cannot exceed 100.
+		</div>
+	</div>
+	<div class="flex">
+		<div class="mt-6 mb-10 h-fit text-center mx-auto">
+			<h2 class="text-xl font-semibold">Gameplay</h2>
+			Mina Arena is inspired by tabletop games like Warhammer and Axis and Allies. The game proceeds
+			in<b>Turns</b>, and each turn is made up of <b>Phases</b>
+		</div>
+	</div>
+	<div>
+		<div class="mt-6 mb-10 h-fit text-center mx-auto">
+			<h2 class="text-xl font-semibold">Phase</h2>
+			Mina Arena is inspired by tabletop games like Warhammer and Axis and Allies. The game proceeds
+			in<b>Turns</b>, and each turn is made up of <b>Phases</b>
+		</div>
+		<div class="flex">
+			<div class="mt-6 mb-10 h-fit text-center mx-auto">
+				<h2 class="text-xl font-semibold">Move</h2>
+				In the movement phase, each unit may move once up to their max movement speed.
+			</div>
+			<div class="mt-6 mb-10 h-fit text-center mx-auto">
+				<h2 class="text-xl font-semibold">Shoot</h2>
+				In the shooting phase, each unit with a ranged attack may shoot at an enemy in range.
+			</div>
+			<div class="mt-6 mb-10 h-fit text-center mx-auto">
+				<h2 class="text-xl font-semibold">Melee</h2>
+				In the melee phase, each unit may attack an enemy which is in melee range/
+			</div>
+		</div>
+	</div>
+	<div>
+		<div class="mt-6 mb-10 h-fit text-center mx-auto">
+			<h2 class="text-xl font-semibold">Turn</h2>
+			Turns alternate back and forth between players. Players sign their actions so that the authenticity
+			of the turns are guaranteed.
+		</div>
+	</div>
+</div>
