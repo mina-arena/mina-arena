af55436badc528cc8398fdfc236887509f19af7f
Coby <contact@45930.xyz>
2023-07-07 16:23:52 -0400

    fix legend (#33)

--- client/src/lib/components/sandbox/arena/ArenaLegend.svelte
+++ client/src/lib/components/sandbox/arena/ArenaLegend.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
-  import { truncateMinaPublicKey } from '$lib/utils';
+	import { truncateMinaPublicKey } from '$lib/utils';
 
-  export let playerPublicKeys: Array<string>;
-  export let playerColors: Array<string>;
-
+	export let playerPublicKeys: Array<string>;
+	export let playerColors: Array<string>;
 </script>
 
 <div class="mr-6">
-  <div>Legend:</div>
-  <br />
-  <div>
-    <div>
-      <div class="w-3 h-3 bg-[{playerColors[0]}] border border-zinc-600" />
-      Player 1: {truncateMinaPublicKey(playerPublicKeys[0])}
-    </div>
-  </div>
-  <br />
-  <div>
-    <div class="w-3 h-3 bg-[{playerColors[1]}] border border-zinc-600" />
-    Player 2: {truncateMinaPublicKey(playerPublicKeys[1])}
-  </div>
+	<div>Legend:</div>
+	<br />
+	<div>
+		<div>
+			<div class="w-3 h-3 bg-primary border border-zinc-600" />
+			Player 1: {truncateMinaPublicKey(playerPublicKeys[0])}
+		</div>
+	</div>
+	<br />
+	<div>
+		<div class="w-3 h-3 bg-secondary border border-zinc-600" />
+		Player 2: {truncateMinaPublicKey(playerPublicKeys[1])}
+	</div>
 </div>
