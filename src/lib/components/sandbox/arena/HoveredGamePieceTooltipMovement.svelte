094d781cc4b25ecbecfb0ddd9005d4836cbfed05
Louis Wheeler <louiswheeleriv@gmail.com>
2023-07-09 16:03:19 -0400

    Refactor tooltip with img and stats

--- src/lib/components/sandbox/arena/HoveredGamePieceTooltipMovement.svelte
+++ src/lib/components/sandbox/arena/HoveredGamePieceTooltipMovement.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { afterUpdate, onMount } from 'svelte';
-  import * as Utils from '../play/utils';
+  import { afterUpdate } from 'svelte';
+  import { onAfterUpdate } from './tooltip-helpers';
+	import HoveredGamePieceTooltipUnitCard from './HoveredGamePieceTooltipUnitCard.svelte';
 
   export let hoveredPiece: GamePiece | undefined;
   export let tooltipAbsolutePosition: Point | undefined;
@@ -8,45 +9,17 @@
   export let playerColors: Array<string>;
 
   afterUpdate(() => {
-    if (hoveredPiece) {
-      showHoveredPieceTooltip();
-    } else {
-      hideHoveredPieceTooltip();
-    }
+    onAfterUpdate(
+      playerPublicKeys,
+      playerColors,
+      hoveredPiece,
+      tooltipAbsolutePosition,
+    );
   });
-
-  // const showHoveredPieceTooltip = (absolutePoint: Point, hoveredPiece: GamePiece) => {
-  const showHoveredPieceTooltip = () => {
-    var tooltip = document.querySelector('#piece-hover-tooltip') as HTMLElement;
-    if (!tooltip || !hoveredPiece || !tooltipAbsolutePosition) return;
-
-    const playerMinaPublicKey = hoveredPiece.gamePlayer.player.minaPublicKey;
-    const playerColor = Utils.playerColor(playerPublicKeys, playerMinaPublicKey, playerColors);
-    
-    tooltip.style.left = (tooltipAbsolutePosition.x + 20) + 'px';
-    tooltip.style.top = (tooltipAbsolutePosition.y + 20) + 'px';
-    tooltip.style.display = 'block';
-    tooltip.style.backgroundColor = playerColor;
-  }
-
-  const hideHoveredPieceTooltip = () => {
-    var tooltip = document.querySelector('#piece-hover-tooltip') as HTMLElement;
-    if (!tooltip) return;
-    
-    tooltip.style.display = 'none';
-  }
-
 </script>
 
 <span id="piece-hover-tooltip" class="p-2 hidden fixed overflow-hidden border border-solid border-black rounded">
   {#if hoveredPiece}
-    {@const hoveredUnit = hoveredPiece.playerUnit.unit}
-    <div>{hoveredPiece.playerUnit.name} ({hoveredUnit.name})</div>
-    <div>HP: {hoveredPiece.health}/{hoveredUnit.maxHealth}</div>
-    <div>Speed: {hoveredUnit.movementSpeed}</div>
-    <div>Armor: {hoveredUnit.armorSaveRoll}+</div>
-    {#if hoveredUnit.rangedNumAttacks}
-      <div>Missile Range: {hoveredUnit.rangedRange}</div>
-    {/if}
+    <HoveredGamePieceTooltipUnitCard {hoveredPiece} />
   {/if}
 </span>
---
+++ src/lib/components/sandbox/arena/HoveredGamePieceTooltipUnitCard.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { imagePathForUnit } from '../play/utils';
+
+  export let hoveredPiece: GamePiece;
+</script>
+
+{#if hoveredPiece}
+  {@const hoveredUnit = hoveredPiece.playerUnit.unit}
+  {@const unitImagePath = imagePathForUnit(hoveredUnit)}
+  <div class="grid grid-cols-2 gap-4mx-auto">
+    <div class="col-span-1">
+      {#if unitImagePath}
+        <img alt="Unit image" src={unitImagePath} width="150" height="150" />
+      {:else}
+        No image for unit
+      {/if}
+    </div>
+    <div class="col-span-1">
+      <div class="text-center">{hoveredPiece.playerUnit.name}</div>
+      <div class="text-center">{hoveredUnit.name}</div>
+
+      <table class="mx-auto">
+        <tr class="[&>*]:px-[4px] [&>*]:text-center">
+          <th><i class="fa-solid fa-person-running-fast"></i></th>
+          <th><i class="fa-solid fa-shield"></i></th>
+          <th><i class="fa-solid fa-heart"></i></th>
+        </tr>
+        <tr class="[&>*]:px-[4px] [&>*]:text-center">
+          <td>{hoveredUnit.movementSpeed}</td>
+          <td>{hoveredUnit.armorSaveRoll}+</td>
+          <td>{hoveredPiece.health}/{hoveredUnit.maxHealth}</td>
+        </tr>
+      </table>
+      <table class="mx-auto mt-[5px]">
+        <tr class="[&>*]:px-[4px] [&>*]:text-center">
+          <th></th>
+          <th><i class="fa-solid fa-hashtag"></i></th>
+          <th><i class="fa-solid fa-bullseye-arrow"></i></th>
+          <th><i class="fa-solid fa-hand-fist"></i></th>
+          <th><i class="fa-solid fa-shield-slash"></i></th>
+          <th><i class="fa-solid fa-heart-crack"></i></th>
+        </tr>
+        <tr class="[&>*]:px-[4px] [&>*]:text-center">
+          <td><i class="fa-solid fa-sword"></i></td>
+          <td>{hoveredUnit.meleeNumAttacks}</td>
+          <td>{hoveredUnit.meleeHitRoll}+</td>
+          <td>{hoveredUnit.meleeWoundRoll}+</td>
+          <td>{hoveredUnit.meleeArmorPiercing}</td>
+          <td>{hoveredUnit.meleeDamage}</td>
+        </tr>
+        {#if hoveredUnit.rangedNumAttacks}
+          <tr class="[&>*]:px-[4px] [&>*]:text-center">
+            <td><i class="fa-solid fa-bow-arrow"></i></td>
+            <td>{hoveredUnit.rangedNumAttacks}</td>
+            <td>{hoveredUnit.rangedHitRoll}+</td>
+            <td>{hoveredUnit.rangedWoundRoll}+</td>
+            <td>{hoveredUnit.rangedArmorPiercing}</td>
+            <td>{hoveredUnit.rangedDamage}</td>
+          </tr>
+        {/if}
+      </table>
+    </div>
+  </div>
+{/if}
