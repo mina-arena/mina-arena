a3cd120e18069291305f2df3eacdd01e05dac623
Coby <contact@45930.xyz>
2023-07-08 20:10:57 -0400

    migrate to client only

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import node from "@sveltejs/adapter-node";
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: node({ env: { port: process.env.PORT } })
 	}
 };
 
