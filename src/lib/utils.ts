1d0ce7ea62d0fd6ffb97db023c4c86bd25795ce5
Louis Wheeler <louiswheeleriv@gmail.com>
2023-07-09 16:55:08 -0400

    Unify image helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,20 @@
+const IMAGE_PATH_BY_UNIT_NAME: Record<string, string> = {
+  'archer': '/images/archer.png',
+  'peasant': '',
+  'swordsman': '',
+  'spearman': '/images/spearman.png',
+  'light cavalry': '',
+  'heavy cavalry': '/images/heavy_cavalry.png',
+  'ballista': '',
+  'hero': '',
+};
+
+const DEFAULT_UNIT_IMAGE_PATH = '/images/archer.png';
+
 export const truncateMinaPublicKey = (key: string, front = 10, back = 10) => {
   return key.slice(0, front) + '...' + key.slice(key.length - back);
 }
 
-export const unitImage = (unitName: string): string => {
-  unitName = unitName.toLowerCase();
-  switch (unitName) {
-    case 'archer':
-      return '/images/archer.png';
-    case 'spearman':
-      return '/images/spearman.png';
-    case 'heavy cavalry':
-      return '/images/heavy_cavalry.png';
-    default:
-      return '/images/archer.png';
-  }
+export const imagePathForUnit = (unit: Unit): string => {
+  return IMAGE_PATH_BY_UNIT_NAME[unit.name.toLowerCase()] || DEFAULT_UNIT_IMAGE_PATH;
 }
