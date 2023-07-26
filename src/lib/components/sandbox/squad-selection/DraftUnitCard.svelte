a43cccf378f37c3936643d33ea5ed323ac98029a
Coby <contact@45930.xyz>
2023-07-25 20:47:07 -0400

    rm armor save roll icon

--- src/lib/components/sandbox/squad-selection/DraftUnitCard.svelte
+++ src/lib/components/sandbox/squad-selection/DraftUnitCard.svelte
@@ -15,21 +15,21 @@
 	class="relative flex flex-col p-4 rounded-2xl bg-stone-900 text-stone-700 col-span-1 cursor-pointer border-t-[1px] border-stone-300"
 >
 	<div class="relative w-full mb-2">
-		<img class="rounded" alt="{unit.name}" src={imagePathForUnit(unit)} />
-		<div class="absolute flex items-center justify-center -left-2 -top-2">
-			<i class="absolute text-[64px] fa-solid fa-shield" title="Armor Save Roll" />
-			<span class="absolute text-[#fff] z-10">{unit.armorSaveRoll}+</span>
-		</div>
+		<img class="rounded" alt={unit.name} src={imagePathForUnit(unit)} />
 		<div class="absolute flex items-center justify-center -right-2 -bottom-2">
 			<i class="absolute text-[64px] fa-solid fa-heart" title="Armor Save Roll" />
 			<span class="absolute text-[#fff] z-10">{unit.maxHealth}</span>
 		</div>
 	</div>
-	<div class="font-almendra-bold uppercase bg-stone-300 p-2 mb-2 rounded text-center">{unit.name}</div>
+	<div class="font-almendra-bold uppercase bg-stone-300 p-2 mb-2 rounded text-center">
+		{unit.name}
+	</div>
 	<div class="bg-stone-300 p-2 rounded flex-grow flex justify-center">
 		<table class="mx-auto">
 			<tr class="[&>*]:px-[3px]">
-				<th class="align-bottom text-center"><i class="fa-solid fa-circle-dollar" title="Points Cost" /></th>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-circle-dollar" title="Points Cost" /></th
+				>
 			</tr>
 			<tr class="[&>*]:px-[3px]">
 				<td class="align-top text-center" title="Points Cost">{unit.pointsCost}</td>
@@ -38,11 +38,21 @@
 		<table class="mx-auto">
 			<tr class="[&>*]:px-[3px]">
 				<th />
-				<th class="align-bottom text-center"><i class="fa-solid fa-hashtag" title="Number of Attacks" /></th>
-				<th class="align-bottom text-center"><i class="fa-solid fa-bullseye-arrow" title="Hit Roll" /></th>
-				<th class="align-bottom text-center"><i class="fa-solid fa-hand-fist" title="Wound Roll" /></th>
-				<th class="align-bottom text-center"><i class="fa-solid fa-shield-slash" title="Armor Piercing" /></th>
-				<th class="align-bottom text-center"><i class="fa-solid fa-heart-crack" title="Damage" /></th>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-hashtag" title="Number of Attacks" /></th
+				>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-bullseye-arrow" title="Hit Roll" /></th
+				>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-hand-fist" title="Wound Roll" /></th
+				>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-shield-slash" title="Armor Piercing" /></th
+				>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-heart-crack" title="Damage" /></th
+				>
 			</tr>
 			<tr class="[&>*]:px-[3px]">
 				<td class="align-top text-center"><i class="fa-solid fa-sword" title="Melee Attacks" /></td>
@@ -54,7 +64,9 @@
 			</tr>
 			{#if unit.rangedNumAttacks}
 				<tr class="[&>*]:px-[3px]">
-					<td class="align-top text-center"><i class="fa-solid fa-bow-arrow" title="Ranged Attacks" /></td>
+					<td class="align-top text-center"
+						><i class="fa-solid fa-bow-arrow" title="Ranged Attacks" /></td
+					>
 					<td class="align-top text-center" title="Number of Attacks">{unit.rangedNumAttacks}</td>
 					<td class="align-top text-center" title="Hit Roll">{unit.rangedHitRoll}+</td>
 					<td class="align-top text-center" title="Wound Roll">{unit.rangedWoundRoll}+</td>
--- src/lib/components/sandbox/squad-selection/MyUnitCard.svelte
+++ src/lib/components/sandbox/squad-selection/MyUnitCard.svelte
@@ -24,21 +24,21 @@
 		: 'cursor-pointer'} {selected ? 'bg-stone-600 scale-105 drop-shadow-lg' : ''}"
 >
 	<div class="relative w-full mb-2">
-		<img class="rounded" alt="{playerUnit.name}" src={imagePathForUnit(playerUnit.unit)} />
-		<div class="absolute flex items-center justify-center -left-2 -top-2">
-			<i class="absolute text-[64px] fa-solid fa-shield" title="Armor Save Roll" />
-			<span class="absolute text-[#fff] z-10">{playerUnit.unit.armorSaveRoll}+</span>
-		</div>
+		<img class="rounded" alt={playerUnit.name} src={imagePathForUnit(playerUnit.unit)} />
 		<div class="absolute flex items-center justify-center -right-2 -bottom-2">
 			<i class="absolute text-[64px] fa-solid fa-heart" title="Armor Save Roll" />
 			<span class="absolute text-[#fff] z-10">{playerUnit.unit.maxHealth}</span>
 		</div>
 	</div>
-	<div class="font-almendra-bold uppercase bg-stone-300 p-2 mb-2 rounded text-center">{playerUnit.name} ({playerUnit.unit.name})</div>
+	<div class="font-almendra-bold uppercase bg-stone-300 p-2 mb-2 rounded text-center">
+		{playerUnit.name} ({playerUnit.unit.name})
+	</div>
 	<div class="bg-stone-300 p-2 rounded flex-grow flex justify-center">
 		<table class="mx-auto">
 			<tr class="[&>*]:px-[3px]">
-				<th class="align-bottom text-center"><i class="fa-solid fa-circle-dollar" title="Points Cost" /></th>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-circle-dollar" title="Points Cost" /></th
+				>
 			</tr>
 			<tr class="[&>*]:px-[3px]">
 				<td class="align-top text-center" title="Points Cost">{playerUnit.unit.pointsCost}</td>
@@ -47,27 +47,49 @@
 		<table class="mx-auto">
 			<tr class="[&>*]:px-[3px]">
 				<th />
-				<th class="align-bottom text-center"><i class="fa-solid fa-hashtag" title="Number of Attacks" /></th>
-				<th class="align-bottom text-center"><i class="fa-solid fa-bullseye-arrow" title="Hit Roll" /></th>
-				<th class="align-bottom text-center"><i class="fa-solid fa-hand-fist" title="Wound Roll" /></th>
-				<th class="align-bottom text-center"><i class="fa-solid fa-shield-slash" title="Armor Piercing" /></th>
-				<th class="align-bottom text-center"><i class="fa-solid fa-heart-crack" title="Damage" /></th>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-hashtag" title="Number of Attacks" /></th
+				>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-bullseye-arrow" title="Hit Roll" /></th
+				>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-hand-fist" title="Wound Roll" /></th
+				>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-shield-slash" title="Armor Piercing" /></th
+				>
+				<th class="align-bottom text-center"
+					><i class="fa-solid fa-heart-crack" title="Damage" /></th
+				>
 			</tr>
 			<tr class="[&>*]:px-[3px]">
 				<td class="align-top text-center"><i class="fa-solid fa-sword" title="Melee Attacks" /></td>
-				<td class="align-top text-center" title="Number of Attacks">{playerUnit.unit.meleeNumAttacks}</td>
+				<td class="align-top text-center" title="Number of Attacks"
+					>{playerUnit.unit.meleeNumAttacks}</td
+				>
 				<td class="align-top text-center" title="Hit Roll">{playerUnit.unit.meleeHitRoll}+</td>
 				<td class="align-top text-center" title="Wound Roll">{playerUnit.unit.meleeWoundRoll}+</td>
-				<td class="align-top text-center" title="Armor Piercing">{playerUnit.unit.meleeArmorPiercing}</td>
+				<td class="align-top text-center" title="Armor Piercing"
+					>{playerUnit.unit.meleeArmorPiercing}</td
+				>
 				<td class="align-top text-center" title="Damage">{playerUnit.unit.meleeDamage}</td>
 			</tr>
 			{#if playerUnit.unit.rangedNumAttacks}
 				<tr class="[&>*]:px-[3px]">
-					<td class="align-top text-center"><i class="fa-solid fa-bow-arrow" title="Ranged Attacks" /></td>
-					<td class="align-top text-center" title="Number of Attacks">{playerUnit.unit.rangedNumAttacks}</td>
+					<td class="align-top text-center"
+						><i class="fa-solid fa-bow-arrow" title="Ranged Attacks" /></td
+					>
+					<td class="align-top text-center" title="Number of Attacks"
+						>{playerUnit.unit.rangedNumAttacks}</td
+					>
 					<td class="align-top text-center" title="Hit Roll">{playerUnit.unit.rangedHitRoll}+</td>
-					<td class="align-top text-center" title="Wound Roll">{playerUnit.unit.rangedWoundRoll}+</td>
-					<td class="align-top text-center" title="Armor Piercing">{playerUnit.unit.rangedArmorPiercing}</td>
+					<td class="align-top text-center" title="Wound Roll"
+						>{playerUnit.unit.rangedWoundRoll}+</td
+					>
+					<td class="align-top text-center" title="Armor Piercing"
+						>{playerUnit.unit.rangedArmorPiercing}</td
+					>
 					<td class="align-top text-center" title="Damage">{playerUnit.unit.rangedDamage}</td>
 				</tr>
 			{/if}
