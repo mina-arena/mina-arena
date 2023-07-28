99fb5bf2e62e9ac355e8387b2824131938b9df9e
graemeblackwood <graeme84@gmail.com>
2023-07-28 07:52:53 +0100

    fix: Styling for legend.

--- src/lib/components/sandbox/arena/ArenaLegend.svelte
+++ src/lib/components/sandbox/arena/ArenaLegend.svelte
@@ -5,18 +5,13 @@
 	export let playerColors: Array<string>;
 </script>
 
-<div class="mr-6">
-	<div>Legend:</div>
-	<br />
-	<div>
-		<div>
-			<div class="w-3 h-3 bg-primary border border-zinc-600" />
-			<div class="active-player">&rarr;</div> Player 1: {truncateMinaPublicKey(playerPublicKeys[0])}
-		</div>
+<div>
+	<div class="flex items-center my-6">
+		<div class="active-player w-6">&rarr;</div>
+		<div class="mx-4 flex-none w-6 h-6 rounded-full bg-primary border border-zinc-600" /> Player 1: {truncateMinaPublicKey(playerPublicKeys[0])}
 	</div>
-	<br />
-	<div>
-		<div class="w-3 h-3 bg-secondary border border-zinc-600" />
-		Player 2: {truncateMinaPublicKey(playerPublicKeys[1])}
+	<div class="flex items-center my-6">
+		<div class="w-6"></div>
+		<div class="mx-4 flex-none w-6 h-6 rounded-full bg-secondary border border-zinc-600" /> Player 2: {truncateMinaPublicKey(playerPublicKeys[1])}
 	</div>
 </div>
