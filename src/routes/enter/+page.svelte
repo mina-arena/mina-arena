4f2f2a7f51a37ebc1cf3aa163aa35ab83d283f0e
graemeblackwood <graeme84@gmail.com>
2023-07-28 06:31:22 +0100

    feat: Styles new game (enter) page and enhance Barracks.

--- src/routes/enter/+page.svelte
+++ src/routes/enter/+page.svelte
@@ -1,16 +1,21 @@
-<div class="p-12">
+<div class="flex-grow p-12 bg-arena-landscape-blurred bg-cover bg-fixed">
 	<h1 class="font-almendra-bold text-4xl my-12 text-center uppercase">Choose your path</h1>
-	<div class="grid grid-cols-2 gap-20 w-3/4 border border-slate-100 mx-auto">
-		<h3 class="text-xl mb-3">Your games in progress:</h3>
-		<p>TODO</p>
-	</div>
-	<div class="w-1/4 border border-slate-300 mx-auto cursor-pointer hover:bg-slate-200">
-		<a href="/sandbox" class="text-center card rounded p-2">
-			<h3 class="text-xl mb-3">New Game (Sandbox)</h3>
-			<p class="text-slate-600">
-				In sandbox mode, you control both sides. It's a good way to learn the game or practice a
-				strategy.
-			</p>
-		</a>
+
+	<div class="flex flex-col items-center lg:flex-row lg:items-stretch justify-center">
+		<div class="flex flex-col justify-center items-center text-center lg:w-1/4 max-w-md p-8 m-8 rounded-xl cursor-pointer bg-white hover:bg-stone-300 bg-opacity-50 drop-shadow-lg">
+			<a href="/sandbox" class="text-center card rounded p-2">
+				<h2 class="font-almendra-bold text-2xl mb-3">New Game (Sandbox)</h2>
+				<div class="text-stone-600">
+					In sandbox mode, you control both sides. It's a good way to learn the game or practice a
+					strategy.
+				</div>
+			</a>
+		</div>
+		<div class="flex flex-col justify-center items-center text-center lg:w-1/4 max-w-md p-8 m-8 rounded-xl bg-white bg-opacity-50 drop-shadow-lg">
+			<h2 class="font-almendra-bold text-2xl mb-3">Games in progress</h2>
+			<div class="text-stone-600">
+				To Do
+			</div>
+		</div>
 	</div>
 </div>
