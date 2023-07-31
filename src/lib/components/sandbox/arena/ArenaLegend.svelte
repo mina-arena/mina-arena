743d87eb392789cde45fe3a01d6f0e496864dd4c
graemeblackwood <graeme84@gmail.com>
2023-07-31 07:08:06 +0100

    feat: Updates player colours and applies rich styling to game UI.

--- src/lib/components/sandbox/arena/ArenaLegend.svelte
+++ src/lib/components/sandbox/arena/ArenaLegend.svelte
@@ -6,23 +6,23 @@
 	export let currentPlayer: string;
 </script>
 
-<div>
-	<div class="flex items-center my-6">
+<div class="w-full my-8">
+	<div class="flag flex bg-[#BD2104] items-center my-6 py-6 text-white drop-shadow-lg">
 		{#if currentPlayer === playerPublicKeys[0]}
-			<div class="active-player w-6">&rarr;</div>
+			<div class="w-12 text-center text-2xl">&#10148;</div>
+			Player 1: {truncateMinaPublicKey(playerPublicKeys[0])}
 		{:else}
-			<div class="w-6" />
+			<div class="w-12" />
+			<span class="opacity-50">Player 1: {truncateMinaPublicKey(playerPublicKeys[0])}</span>
 		{/if}
-		<div class="mx-4 flex-none w-6 h-6 rounded-full bg-primary border border-zinc-600" />
-		Player 1: {truncateMinaPublicKey(playerPublicKeys[0])}
 	</div>
-	<div class="flex items-center my-6">
+	<div class="flag flex bg-[#003CB0] items-center my-6 py-6 text-white drop-shadow-lg">
 		{#if currentPlayer === playerPublicKeys[1]}
-			<div class="active-player w-6">&rarr;</div>
+			<div class="w-12 text-center text-2xl">&#10148;</div>
+			Player 2: {truncateMinaPublicKey(playerPublicKeys[1])}
 		{:else}
-			<div class="w-6" />
+			<div class="w-12" />
+			<span class="opacity-50">Player 2: {truncateMinaPublicKey(playerPublicKeys[1])}</span>
 		{/if}
-		<div class="mx-4 flex-none w-6 h-6 rounded-full bg-secondary border border-zinc-600" />
-		Player 2: {truncateMinaPublicKey(playerPublicKeys[1])}
 	</div>
 </div>
--- src/lib/components/sandbox/play/Arena.svelte
+++ src/lib/components/sandbox/play/Arena.svelte
@@ -6,7 +6,7 @@
 	export let game: Game;
 
 	const legendConfig = {
-		colors: ['#EF626C', '#84DCCF']
+		colors: ['#BD2104', '#003CB0']
 	};
 
 	const playerPublicKeys = (game.gamePlayers || []).map((p) => p.player.minaPublicKey) || ['', ''];
@@ -17,17 +17,27 @@
 <div class="flex-grow flex bg-arena-landscape-blurred bg-cover">
 	<ArenaCanvas {game} playerColors={legendConfig.colors} {rerender} />
 	<div
-		class="flex flex-col items-center bg-stone-300 max-w-[33vw] 2xl:max-w-[25vw] p-6 pt-9 m-16 border-[8px] border-stone-900 rounded-[32px]"
-		style="box-shadow: 0 0 60px #000"
+		class="flex flex-col items-center max-w-[33vw]"
 	>
-		<h1 class="font-almendra-bold uppercase m-0">Turn {game.turnNumber}</h1>
-		<h2 class="font-almendra-bold uppercase text-3xl mt-2">{game.currentPhase?.name} PHASE</h2>
+		<div class="w-full pr-16">
+			<div class="w-full px-12 py-8 bg-stone-700 text-center text-white rounded-br-2xl rounded-bl-2xl drop-shadow-lg">
+				<h1 class="font-almendra-bold uppercase m-0">Turn {game.turnNumber}</h1>
+				<h2 class="font-almendra-bold uppercase text-3xl mt-2">{game.currentPhase?.name} PHASE</h2>
+			</div>
+		</div>
+
 		<ArenaLegend
 			{playerPublicKeys}
 			playerColors={legendConfig.colors}
 			currentPlayer={game.currentPhase?.gamePlayer.player.minaPublicKey || ''}
 		/>
-		<ArenaActionLog {game} {playerPublicKeys} playerColors={legendConfig.colors} />
+
+		<div class="flex-grow flex w-full pr-16">
+			<div class="w-full flex-grow flex flex-col action-log-scroll">
+				<div class="scroll-top">&nbsp;</div>
+				<ArenaActionLog {game} {playerPublicKeys} playerColors={legendConfig.colors} />
+			</div>
+		</div>
 	</div>
 </div>
 <div style="display: none;">
