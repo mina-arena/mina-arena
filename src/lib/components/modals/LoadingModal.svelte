1b1138357501944022cfd6ce146fe9c1b4824eef
Coby <contact@45930.xyz>
2023-08-03 21:54:26 -0400

    require unit name

---
+++ src/lib/components/modals/LoadingModal.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { closeModal } from 'svelte-modals';
+
+	// provided by Modals
+	export let isOpen: boolean;
+</script>
+
+{#if isOpen}
+	<div class="modal"><i class="fa fa-solid fa-refresh fa-spin" /></div>
+{/if}
